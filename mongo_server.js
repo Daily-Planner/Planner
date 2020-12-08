40e66b73f629a0d0770c3540efbe5bcaf0873de6
jinsik kim <rlawlstlr9312@gmail.com>
2020-12-08 15:02:44 -0600

    server 1st

--- mongo_server.js
+++ mongo_server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const gongoose = require('mongoose');
+const Users = require("./db/user")
 const path = require('path');
 const PORT = 3001;
 const app = express();
+
+gongoose.connect('mongodb+srv://sickdata:sick@sick1.mfjtw.mongodb.net/<dbname>?retryWrites=true&w=majority',  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.use(express.urlencoded({extended:true}));
 
@@ -15,8 +18,34 @@ app.get("/login/:user/:pass", function(req, res){
     var name = req.params.user;
     var password = req.params.pass;
 
+    Users.findOne({name: new RegExp(name, 'i')})
+    .then(function(data){
+        if(data.password===password) {
+            res.json("Successfully logged in");
+        } else{
+            res.json("Incorrect Password")
+        }
+    })
+
     
 
+})
+
+app.post("/signUp", (req,res)=>{
+
+    var newUser= {
+        name:req.body.name,
+        password:req.body.password,
+    }
+    console.log("in post request");
+    Users.create(newUser)
+    .then(function(data){
+       
+        res.json("User Created");
+    })
+    .catch(function(err){
+        res.json(err);
+    })
 })
 
 
@@ -26,30 +55,6 @@ app.get("/login/:user/:pass", function(req, res){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`server is running and listening`);
+    console.log('server is running and listening');
 });
